becc7c7a475bd8e202f373cebccf462190491161
seon1234 <kimseonho4@gmail.com>
2020-11-11 17:17:16 +0100

    Update 4.5.py

--- 4.5.py
+++ 4.5.py
@@ -5,4 +5,4 @@ def read_excel(): #Working on this one -Saviër
 # For this function, I would like the forces F_x and F_z to have coordinates that I can use
 def calculate_equivalent_FM(forces_and_moments_dictionary, CG_coordinates): # Working on this one -Kristian
     #The forces in the x- and z-directions will just be the forces
-    
+    return { "F_cgx": forces_and_moments_dictionary["F_x"], "F_cgz": forces_and_moments_dictionary["F_z"]}
