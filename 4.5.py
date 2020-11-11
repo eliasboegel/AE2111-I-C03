649dc0b19b7247a99544851b2476e3f5fc68c7c6
seon1234 <kimseonho4@gmail.com>
2020-11-11 17:10:09 +0100

    Update 4.5.py

--- 4.5.py
+++ 4.5.py
@@ -1,5 +1,7 @@
 print("Hello World")
 
 
-
-def calculate_equivalent_FM(): # Working on this one -Kristian
+# For this function, I would like the forces F_x and F_z to have coordinates that I can use
+def calculate_equivalent_FM(forces_and_moments_dictionary, CG_coordinates): # Working on this one -Kristian
+    #The forces in the x- and z-directions will just be the forces
+    
