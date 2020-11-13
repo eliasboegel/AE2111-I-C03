01ffc6b51897e54d6ddda1a06505889f3f9852e7
seon1234 <kimseonho4@gmail.com>
2020-11-13 09:21:08 +0100

    Update 4.5.py
    Started work on function in_plane_force

--- 4.5.py
+++ 4.5.py
@@ -6,4 +6,8 @@ def read_excel(): #Working on this one -Saviër
 # CG should be coordinates as (x, z)
 def calculate_equivalent_FM(forces_and_moments_dictionary, CG_coordinates): # Working on this one -Kristian
     #The forces in the x- and z-directions will just be the forces
-    return { "F_cgx": forces_and_moments_dictionary["F_x"], "F_cgz": forces_and_moments_dictionary["F_z"], "M_cgy": forces_and_moments_dictionary["F_x"] * CG_coordinates(1) - forces_and_moments_dictionary["F_z"] * CG_coordinates(0)}
+    return { "F_cgx": forces_and_moments_dictionary["F_x"], "F_cgz": forces_and_moments_dictionary["F_z"], "M_cgy": forces_and_moments_dictionary["F_x"] * (forces_and_moments_dictionary["coord_z"] - CG_coordinates(1))  - forces_and_moments_dictionary["F_z"] * (forces_and_moments_dictionary["coord_x"] - CG_coordinates(0))}
+
+
+def in_plane_force(forces_and_moments_dictionary, fastener_list): # Working on this one -Kristian
+    
