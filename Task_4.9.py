7cb44e9691b3e70a01265785300bcc5340e69781
Fenneline <74305640+Fenneline@users.noreply.github.com>
2020-11-20 10:58:49 +0100

    Add files via upload

---
+++ Task_4.9.py
@@ -0,0 +1,16 @@
+from math import pi
+
+#calculating the normal stress
+D_fo = 4
+D_fi = 2
+r_fo = D_fo / 2
+r_fi = D_fi / 2
+
+F_y = 4 # pull-through load
+
+A = pi * ( r_fo**2 - r_fi**2 )
+
+stress_N = F_y / A
+
+print(stress_N)
+
