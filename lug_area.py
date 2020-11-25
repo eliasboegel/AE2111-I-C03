aa28178cce57338547b4feca2248aef8428cf57d
saffsaffsaff <44726112+saffsaffsaff@users.noreply.github.com>
2020-11-25 13:39:26 +0100

    Update lug_area.py

--- lug_area.py
+++ lug_area.py
@@ -3,15 +3,18 @@ pi = math.pi
 from dimensions import *
 
 
+A_front = 2 * (w * t1) + 3* (h * w) - 4 * pi * (0.5*D2)**2
+
+A_sunlitside = t2 * w + l * w + 0.5 * pi * (0.5*w)**2 - pi * (0.5*D1)**2
+
+A_top = h * t2 * 3 + (l + 0.5 * w) * t1
+
+A_inside = l*w+0.5*pi*(0.5*w)**2 - pi*(0.5*D1)**2
+
+A_back = w * (3*h+2*t1)
+
+A_total = A_sunlitside*2 + A_top *2 + A_inside * 2 + A_back + A_front
 
 
-A_side = t2 * w + w * (l-0.5*D1) + (pi*(0.5*w)**2)-(pi*(D1*0.5)**2)
-
-A_neg_solararray = 2 * (w * t1) + (h*w)*3 - 4*pi*(0.5*D2)**2 #done
-
-A_top_bottom = ((3*(h*t2)+2*t1*t2)+l*t1*2)*2
-
-
-A_total = 2*A_side + A_neg_solararray + A_top_bottom
 
 
