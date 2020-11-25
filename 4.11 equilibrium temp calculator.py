6e42d48994350d7d7731e49a8d30d682ab0c4c0e
NoaS2001 <74305591+NoaS2001@users.noreply.github.com>
2020-11-25 14:21:47 +0100

    Add files via upload

--- 4.11 equilibrium temp calculator.py
+++ 4.11 equilibrium temp calculator.py
@@ -23,17 +23,20 @@ sigma = 5.67*10**(-8)
 
 """ emitting and projected area of lug """
 #calculations of the area
-A_side = t2 * w + w * (l-0.5*D1) + (pi*(0.5*w)**2)-(pi*(D1*0.5)**2)
+A_front = 2 * (w * t1) + 3* (h * w) - 4 * pi * (0.5*D2)**2
 
-A_neg_solararray = 2 * (w * t1) + (h*w)*3 - 4*pi*(0.5*D2)**2 #done
+A_sunlitside = t2 * w + L * w + 0.5 * pi * (0.5*w)**2 - pi * (0.5*D1)**2
 
-A_top_bottom = ((3*(h*t2)+2*t1*t2)+l*t1*2)*2
+A_top = h * t2 * 3 + (L + 0.5 * w) * t1
+
+A_inside = L*w+0.5*pi*(0.5*w)**2 - pi*(0.5*D1)**2
+
+A_back = w * (3*h+2*t1)
+
+A_total = A_sunlitside*2 + A_top *2 + A_inside * 2 + A_back + A_front
 
 
-A_total = 2*A_side + A_neg_solararray + A_top_bottom
-
-
-A_i = A_side  #projected area
+A_i = A_sunlitside  #projected area
 A_e = A_total  #emitting area
 
 
