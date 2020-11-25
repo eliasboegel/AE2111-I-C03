abab1fbc35dc9dae2d549ce6165836a047ab6c41
Fenneline <74305640+Fenneline@users.noreply.github.com>
2020-11-25 10:21:36 +0100

    Update Task_4.9.py

--- Task_4.9.py
+++ Task_4.9.py
@@ -1,23 +1,43 @@
 from math import pi
 
 #calculating the normal stress
-D_fo = 4 #outer diameter
-D_fi = 2 #inner diameter
+D_fo = 7 #outer diameter in mm
+D_fi = 4 #inner diameter in mm
 r_fo = D_fo / 2
 r_fi = D_fi / 2
 
 F_y = 4 # pull-through load
 
-A = pi * ( r_fo**2 - r_fi**2 ) #area of the fastener head on the attached parts
+A_head_ap = pi * ( r_fo**2 - r_fi**2 ) #area of the fastener head on the attached parts
 
-stress_N = F_y / A
+stress_N = F_y / A_head_ap
 
 print(stress_N)
 
-#calculating shaer stress
+#calculating shaer stress for t1
+t1 = 2
+A_t1_ap = pi * t1 * D_fi #area of the attached parts of t1
+stress_t1_V = F_y / A_t1_ap
 
-t = 2
-A_ap = pi * t * D_fi #area of the attached parts
-stress_V = F_y / A_ap
+print('shearstress in t1 = ',stress_t1_V)
 
-print(stress_V)
+#calculating shaer stress for t2
+t2 = 2
+A_t2_ap = pi * t2 * D_fi #area of the attached parts of t2
+stress_t2_V = F_y / A_t2_ap
+
+print('shearstress in t2 = ',stress_t2_V)
+
+#calculating shaer stress for t3
+t3 = 2
+A_t3_ap = pi * t3 * D_fi #area of the attached parts of t3
+stress_t3_V = F_y / A_t3_ap
+
+print('shearstress in t3 = ',stress_t3_V)
+
+#total shear stress
+
+stress_V_tot = stress_t1_V + stress_t2_V + stress_t3_V
+
+print('total shearstress = ',stress_V_tot)
+
